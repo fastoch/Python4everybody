4ab64f3ef6b8f897adeb3bee6f80931b6c684024
fastoch <fastoch@ik.me>
2023-02-05 20:09:15 +0100

    TODO

--- largest_num.py
+++ largest_num.py
@@ -1,10 +1,7 @@
+# doesn't work if all the numbers in my list are negative 
 max = 0
-for i in [9,74,51,-12,-82,0]:
-    if i < 0:
-        continue
-    elif i > max:
+for i in [-20,-80,-10,-65,-32]:
+    if i > max:
         max = i
-if max == 0:
-    print("All numbers in this list are negative or equal to zero")
-else:
-    print("The largest number in the list is: ",max)
+    
+print("The largest number in the list is: ",max)
---
+++ smallest_num.py
@@ -0,0 +1,2 @@
+# doesn't work if all the numbers in my list are positive 
+min = 0

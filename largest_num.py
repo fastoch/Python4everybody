7578333aa237e3367407e2935987e2076079e92e
fastoch <fastoch@ik.me>
2023-02-05 17:58:36 +0100

    largest number improved

--- largest_num.py
+++ largest_num.py
@@ -1,5 +1,10 @@
 max = 0
-for i in [9,74,51,-12,-82]:
-    if i > max:
+for i in [9,74,51,-12,-82,0]:
+    if i < 0:
+        continue
+    elif i > max:
         max = i
-print("The largest number of the list is: ",max)
+if max == 0:
+    print("All numbers in this list are negative or equal to zero")
+else:
+    print("The largest number in the list is: ",max)

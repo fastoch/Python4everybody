700bb1b15187c07982b81a33fd301c394cc9eb39
fastoch <fastoch@ik.me>
2023-02-15 18:48:21 +0100

    *

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@184/820min 
+@185/820min 
--- looping-through-strings.py
+++ looping-through-strings.py
@@ -4,3 +4,12 @@ while index < len(fruit):
     letter = fruit[index]
     print(index, letter)
     index += 1
+
+print()
+
+# same thing with a for loop
+fruit = "pineapple"
+index = 0
+for letter in fruit:
+    print(index, letter)
+    index += 1

c047e0de18c8d348f437d359b1e2e6ce8bf0975d
fastoch <fastoch@ik.me>
2023-02-15 22:13:23 +0100

    *

---
+++ string_comparison.py
@@ -0,0 +1,11 @@
+word = input('Enter a word: ')
+
+if word == 'banana':
+    print('All right, bananas!')
+
+if word < 'banana':
+    print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana':
+    print('Your word, ' + word + ', comes after banana.')
+else:
+    print('All right, bananas!')

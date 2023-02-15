37618e4fba9373bec072f8a35bf8ea3c8ae03e5b
fastoch <fastoch@ik.me>
2023-02-15 20:49:54 +0100

    *

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@185/820min 
+@186/820min 
--- looping-through-strings.py
+++ looping-through-strings.py
@@ -2,14 +2,12 @@ fruit = 'banana'
 index = 0
 while index < len(fruit):
     letter = fruit[index]
-    print(index, letter)
+    print(letter)
     index += 1
 
 print()
 
-# same thing with a for loop
+# same thing with a for loop (iteration variable is implicit)
 fruit = "pineapple"
-index = 0
 for letter in fruit:
-    print(index, letter)
-    index += 1
+    print(letter)

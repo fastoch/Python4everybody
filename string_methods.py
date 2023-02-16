c3a35330c0d5b6382a23ee918cee9167b42b69a5
fastoch <fastoch@ik.me>
2023-02-16 10:40:20 +0100

    *

--- string_methods.py
+++ string_methods.py
@@ -4,8 +4,30 @@ lowerCase = stuff.lower()
 print(lowerCase)
 print()
 
+upperCase = stuff.upper()
+print(upperCase)
+print()
+
 print(type(stuff))
 print()
 
 # list all methods that can be applied to my string
 print(dir(stuff))
+print()
+
+# Search & Replace
+greet = 'Hello Bob!'
+nstr = greet.replace('Bob','Jane')
+print(nstr)
+nstr = greet.replace('o','X')
+print(nstr)
+print()
+
+# Stripping whitespaces
+greet2 = '   Hello Bob   '
+print(greet2+'!')
+print(greet2.lstrip()+'!')
+print(greet2.rstrip()+'!')
+print(greet2.strip()+'!')
+print()
+

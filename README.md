bf691a6c40c40005f4f52163193221e9e298e8ae
fastoch <fastoch@ik.me>
2023-02-16 10:08:10 +0100

    *

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@195/820min 
+@200/820min 
---
+++ string_methods.py
@@ -0,0 +1,11 @@
+stuff = 'Hello World!'
+
+lowerCase = stuff.lower()
+print(lowerCase)
+print()
+
+print(type(stuff))
+print()
+
+# list all methods that can be applied to my string
+print(dir(stuff))

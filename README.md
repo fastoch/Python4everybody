d07ce8e03d93825f261e20d9238410be40000ba0
fastoch <fastoch@ik.me>
2023-02-16 21:46:19 +0100

    *

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@220/820min 
+@223/820min 
 Chapter 7 - Reading Files
--- read_a_file.py
+++ read_a_file.py
@@ -1,7 +1,16 @@
 xfile = open('/home/fastoch/Documents/test.txt')
 for line in xfile:
     print(line, end="") 
-    # end="" cancels the automatic addition of a newline
+    # end="" deletes additional newlines at the end of each line
+    # because the print() function adds a newline to those already existing in the file
+
+print()
+
+# alternative to using end=""
+xfile = open('/home/fastoch/Documents/test.txt')
+for line in xfile:
+    line = line.rstrip() # each newline is considered a whitespace and therefore is stripped
+    print(line) 
 
 print()
 
@@ -10,3 +19,5 @@ xfile = open('/home/fastoch/Documents/test.txt')
 inp = xfile.read()
 print(len(inp)) # print the length of the file (number of characters)
 print(inp[:25]) # print the 25 first characters in the file
+
+print()

205d0199dbaaaa238410140033c1abb6b3574c72
fastoch <fastoch@ik.me>
2023-02-16 18:15:49 +0100

    *

--- read_a_file.py
+++ read_a_file.py
@@ -2,3 +2,11 @@ xfile = open('/home/fastoch/Documents/test.txt')
 for line in xfile:
     print(line, end="") 
     # end="" cancels the automatic addition of a newline
+
+print()
+
+# Reading the whole file
+xfile = open('/home/fastoch/Documents/test.txt')
+inp = xfile.read()
+print(len(inp)) # print the length of the file (number of characters)
+print(inp[:25]) # print the 25 first characters in the file

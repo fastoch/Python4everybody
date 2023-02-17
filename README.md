fdc90804fc6c2ed9fd6d247e6a311bcb07a3c0c7
fastoch <fastoch@ik.me>
2023-02-17 11:35:33 +0100

    *

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@230/820min 
+@240/820min 
 Chapter 7 - Reading Files
---
+++ lists.py
@@ -0,0 +1,6 @@
+# lists are the simplest data structures
+# a list is a type of collection
+friends = ['Sandro','Aurélie','Timothée']
+for friend in friends:
+    print('Happy Birthday',friend)
+print('Done!')

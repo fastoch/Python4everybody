26a09882dd828b3c19e1c40efa186afa4d737f8c
fastoch <fastoch@ik.me>
2023-02-17 08:24:07 +0100

    *

--- read_a_file.py
+++ read_a_file.py
@@ -4,7 +4,7 @@ for line in xfile:
     # end="" deletes additional newlines at the end of each line
     # because the print() function adds a newline to those already existing in the file
 
-print()
+print('\n'+'*'*64+'\n')
 
 # alternative to using end=""
 xfile = open('/home/fastoch/Documents/test.txt')
@@ -12,7 +12,7 @@ for line in xfile:
     line = line.rstrip() # each newline is considered a whitespace and therefore is stripped
     print(line) 
 
-print()
+print('\n'+'*'*64+'\n') # separator
 
 # Reading the whole file
 xfile = open('/home/fastoch/Documents/test.txt')
@@ -20,4 +20,13 @@ inp = xfile.read()
 print(len(inp)) # print the length of the file (number of characters)
 print(inp[:25]) # print the 25 first characters in the file
 
-print()
+print('\n'+'*'*64+'\n')
+
+# skip a line with continue
+xfile = open('/home/fastoch/Documents/test.txt')
+for line in xfile:
+    if not line.startswith('spirit'):
+        continue
+    print(line, end="")
+
+print('\n'+'*'*64+'\n')

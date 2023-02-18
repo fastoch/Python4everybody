cc3f1083c289043434c3b94888f84ad6dc7fe08e
fastoch <fastoch@ik.me>
2023-02-18 17:54:41 +0100

    more splitting

--- lists.py
+++ lists.py
@@ -60,4 +60,18 @@ print(stuff)
 print(stuff[0])
 for w in stuff:
     print(w)
-    
+
+# When you do not specify a delimiter, multiple spaces are treated like one delimiter.
+# You can specify what delimiter to use in the splitting
+line = 'A lot           of spaces'
+etc = line.split()
+print(etc)
+
+line = 'first;second;third'
+thing = line.split()
+print(thing)
+print(len(thing))
+
+thing = line.split(';')
+print(thing)
+print(len(thing))

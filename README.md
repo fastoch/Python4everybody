5466ff8be67a7341d26b39378555503f5516fa32
fastoch <fastoch@ik.me>
2023-02-18 10:23:44 +0100

    building a list from scratch

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@240/820min 
+@245/820min 
 Chapter 7 - Reading Files
--- lists.py
+++ lists.py
@@ -4,3 +4,31 @@ friends = ['Sandro','Aurélie','Timothée']
 for friend in friends:
     print('Happy Birthday',friend)
 print('Done!')
+
+# Concatenating lists using +
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print(c) # will print [1,2,3,4,5,6]
+
+# List can be sliced using :
+t = [9,41,12,3,74,15]
+print(t[1:3])
+print(t[:4])
+print(t[3:])
+# just like in strings, the second number is "up to but not including"
+
+# List Methods
+x = list() # creates an empty list
+print(x)
+print(type(x))
+print(dir(x)) # list all methods available for my list
+
+# Building a list from scratch
+stuff = list()
+stuff.append('book')
+stuff.append(99)
+print(stuff)
+stuff.append('cookie')
+print(stuff)
+

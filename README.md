05add5382b85d1a66d16dd47c5f06dc320568158
fastoch <fastoch@ik.me>
2023-02-19 18:44:59 +0100

    dictionaries

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@270/820min 
+@273/820min 
 Chapter 9 - Dictionaries
---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+print(purse)
+print(purse['candy'])
+purse['candy'] += 2
+print(purse)
+
+print('*'*64)
+

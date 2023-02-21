6d4f1a6a335e9627d84a2e627d66a088285f2a48
fastoch <fastoch@ik.me>
2023-02-21 21:17:03 +0100

    using dictionaries for counting

--- dictionaries.py
+++ dictionaries.py
@@ -9,3 +9,23 @@ print(purse)
 
 print('*'*64)
 
+# Comparing lists and dictionaries
+# Dictionaries are like lists except that they use keys instead of numbers to look up values
+lst = list()
+lst.append(21)
+lst.append(183)
+print(lst)
+lst[0] = 23
+print(lst)
+
+ddd = dict()
+ddd['age'] = 21
+ddd['course'] = 182
+print(ddd)
+ddd['age'] = 23
+print(ddd)
+
+print('*'*64)
+
+# One common use of dictionaries is counting how often we see something
+

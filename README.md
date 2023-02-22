f297b6d87e121272b2a71108a40bb2c9d162e01d
fastoch <fastoch@ik.me>
2023-02-22 19:03:34 +0100

    *

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@287/820min 
+@290/820min 
 Chapter 9 - Dictionaries
--- dictionaries.py
+++ dictionaries.py
@@ -64,5 +64,9 @@ line = input('Enter a line of text: ')
 words = line.split()
 print('Words:', words)
 
+print('Counting...')
+for word in words:
+    counts[word] = counts.get(word, 0) + 1
+print('Counts', counts)
 
 

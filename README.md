b8101b3165c40a224223c7435a0e8a8b74a85325
fastoch <fastoch@ik.me>
2023-02-23 20:38:30 +0100

    *

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@303/820min 
+@314/820min 
 Chapter 9 - Dictionaries
--- dictionaries2.py
+++ dictionaries2.py
@@ -3,10 +3,10 @@ fname = input('Enter file: ')
 if len(fname) < 1 : fname = 'clown.txt'
 hand = open(fname)
 
+counts = dict()
 for line in hand:
-    line = line.rstrip() # remove whitespace off the right-hand side
-    # print(line)
-    wds = line.split()
-    # print(wds)
-    for w in wds:
-        print(w)
+    line = line.rstrip() # remove whitespace from the right-hand side
+    wds = line.split() # turn each line into a list of words
+    for w in wds: # for each word in my file
+        counts[w] = counts.get(w,0) + 1 
+print(counts)

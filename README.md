02f71eb9e97552205b541839174fd676d737af9e
fastoch <fastoch@ik.me>
2023-02-25 15:19:13 +0100

    sorting a list of tuples by key + by value

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@330/820min 
+@336/820min 
 Chapter 10 - Tuples
--- tuples.py
+++ tuples.py
@@ -34,3 +34,27 @@ print()
 # The items() method in dictionaries returns a list of (key,value) tuples
 tups = d.items()
 print(tups)
+
+print()
+
+# Sorting lists of tuples
+d = {'a':10, 'b':1, 'c':22, 'd':16, 'e':8}
+# First, we sort the sort the dictionary by key using the items() method and sorted() function
+keySorted = sorted(d.items())
+print('Sorted by key:', keySorted)
+# NB: the sorted() function does not seem necessary, as the items() method already returns a sorted list of tuples
+for k,v in d.items():
+    print(k, v)
+
+print()
+
+# Sorting by value instead of key
+valueSorted = list()
+for k,v in d.items():
+    valueSorted.append((v,k))
+print('(value, key) instead of (key, value):', valueSorted)
+valueSorted = sorted(valueSorted)
+print('Sorted by value:', valueSorted)
+valueSorted = sorted(valueSorted, reverse=True)
+print('Sorted by value and reversed:', valueSorted)
+

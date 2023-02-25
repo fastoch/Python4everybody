b79d7cbe33f66517361c46a60a3be0c86fa4cad0
fastoch <fastoch@ik.me>
2023-02-25 17:14:31 +0100

    top 10 most common words

--- tuples.py
+++ tuples.py
@@ -60,4 +60,25 @@ print('Sorted by value:', valueSorted)
 valueSortedReversed = sorted(valueSorted, reverse=True)
 print('Sorted by value and reversed:', valueSortedReversed)
 
+print()
+
 # The top 10 most common words
+fhand = open('romeo-full.txt') # open my file
+counts = dict() # create a dictionary for storing each word's count
+for line in fhand:
+    words = line.split() # turn each line into a list of words
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1 # initialize or increment counting
+# We transfer our dictionary entries into a list of tuples
+lst = list() 
+for key, val in counts.items():
+    newTuple = (val, key) # value becomes first element of each tuple, so that we can later sort by value
+    lst.append(newTuple)
+
+lst = sorted(lst, reverse=True) # sorting our list by value and reversing the order so that biggest values come first
+# We print out top 10 most common words and the corresponding count
+for val, key in lst[:10]:
+    print(key, val)
+
+
+# The split() method splits a string into a list. You can specify the separator, default separator is any whitespace.

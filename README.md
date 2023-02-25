d241bf493e7c49372a70d64e177b10562fc73c5b
fastoch <fastoch@ik.me>
2023-02-25 10:54:09 +0100

    tuples methods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI  
 
-@323/820min 
+@326/820min 
 Chapter 10 - Tuples
---
+++ tuples.py
@@ -0,0 +1,17 @@
+tup = ('Timothée', 'Sandro', 'Aurélie', 'Fabrice')
+print(tup)
+print(tup[2])
+
+y = (1, 6 , 8, 2)
+print(y)
+print(max(y))
+
+l = list()
+print('Methods you can use with a list:')
+print(dir(l))
+
+print()
+
+t = tuple()
+print('Methods you can use with a tuple:')
+print(dir(t))

ce489d170176911f0ccd1b2e2a145f101c2c2c7c
fastoch <fastoch@ik.me>
2023-03-02 09:43:55 +0100

    *

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://www.youtube.com/watch?v=8DvywoWv6fI  
+https://www.youtube.com/watch?v=8DvywoWv6fI
 
-@347/820min 
+@350/820min
 Chapter 10 - Tuples
--- tuples.py
+++ tuples.py
@@ -90,7 +90,7 @@ print()
 fhand = open('romeo-full.txt') 
 counts = dict() 
 for line in fhand:
-    words = line.split() 
+    words = line.split() # default separator = whitespace
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 reversedSortedTuple = sorted([(v,k) for k,v in counts.items()], reverse=True)

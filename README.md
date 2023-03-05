8809f83a1e48d0cff3850d0c8fa1c5b3ec7b4a29
fastoch <fastoch@ik.me>
2023-03-05 09:46:41 +0100

    from matching to extracting data

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 https://www.youtube.com/watch?v=8DvywoWv6fI
 
-@360/820min
+@365/820min
 Chapter 11 - Regular Expressions
--- RegEx.py
+++ RegEx.py
@@ -23,3 +23,12 @@ for line in hand:
         print(line)
         count += 1
 print("Number of lines containing 'From':", count)
+
+print()
+
+# From matching to extracting data
+import re
+
+x = "My 2 favorite numbers are 16 and 82"
+y = re.findall("[0-9]+", x)
+print(y)

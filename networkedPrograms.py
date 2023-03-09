ce83a70d8d2fb85926d60bcf99e0f8c9393f3618
fastoch <fastoch@ik.me>
2023-03-09 17:25:29 +0100

    basic web browser

--- networkedPrograms.py
+++ networkedPrograms.py
@@ -5,9 +5,10 @@ mysock.connect('data.pr4e.org', 80) # extend the socket and connect it to a web 
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode() # make a request and assign it to a variable
 mysock.send(cmd) # send the request
 
+# Prepare to receive data from the web server
 while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode())
-mysock.close()
+    data = mysock.recv(512) # receive 512 characters at a time
+    if len(data) < 1:       # if zero data comes back, 
+        break               # break the loop...
+    print(data.decode())    # decode data and display the web page
+mysock.close()              # ... and close the socket

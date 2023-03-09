b4d9af8f4fb03f2a65810240b8a26bf6cac7b595
fastoch <fastoch@ik.me>
2023-03-09 18:11:50 +0100

    replacing \n\n with \r\n\r\n

--- networkedPrograms.py
+++ networkedPrograms.py
@@ -2,7 +2,7 @@ import socket # import required library
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # make a socket
 mysock.connect(('data.pr4e.org', 80)) # extend the socket and connect it to a web server on port 80
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode() # make a request and assign it to a variable
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode() # make a request and assign it to a variable
 mysock.send(cmd) # send the request
 
 # Prepare to receive data from the web server
